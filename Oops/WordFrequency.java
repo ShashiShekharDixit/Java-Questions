af0682654b8958fe9264f67444417573de26be13
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2025-09-24 23:48:47 +0530

    update

--- Oops/WordFrequency.java
+++ Oops/WordFrequency.java
@@ -1,9 +1,9 @@
 import java.util.HashMap;
 
-public class WordFrequencySimple {
+public class WordFrequency{
     public static void main(String[] args) {
         String text = "Hello world! Hello Java. Java is fun, and Java is powerful.";
-        text = text.toLowerCase().replaceAll("[^a-z0-9 ]", "");
+        text = text.toLowerCase().replaceAll("[^a-z A-Z 0-9 ]", "");
 
         String[] words = text.split(" ");
         HashMap<String, Integer> map = new HashMap<>();
